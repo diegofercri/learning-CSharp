5b11099f622096080733a9708ec17e1905e975da
Diego Fernández Criado <diegofercri25@gmail.com>
2024-11-11 08:37:00 +0100

    add Safari

---
+++ SafariApp/Form1.cs
@@ -0,0 +1,36 @@
+namespace SafariApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        internal Form1(Controller controller)
+        {
+            this.controller = controller;
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
